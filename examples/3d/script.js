0320d9adce1d636eff2b75986ae5f82aff2bf5f8
Zack <zack@ukaton.com>
2024-03-01 09:23:53 -0800

    setup

---
+++ examples/3d/script.js
@@ -0,0 +1,7 @@
+import BrilliantSole from "../../src/BrilliantSole.js";
+window.BrilliantSole = BrilliantSole;
+console.log({ BrilliantSole });
+
+const brilliantSole = new BrilliantSole();
+console.log({ brilliantSole });
+window.brilliantSole = brilliantSole;
--- examples/test/script.js
+++ examples/test/script.js
@@ -1,9 +1,9 @@
 import BrilliantSole from "../../src/BrilliantSole.js";
 window.BrilliantSole = BrilliantSole;
-console.log("BrilliantSole", BrilliantSole);
+console.log({ BrilliantSole });
 
 const brilliantSole = new BrilliantSole();
-console.log("brilliantSole", brilliantSole);
+console.log({ brilliantSole });
 window.brilliantSole = brilliantSole;
 
 // CONNECTION
