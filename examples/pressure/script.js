e79f9c0e4655d1a0f8fca4085d68d309ede47af4
Zack <zack@ukaton.com>
2024-03-06 11:10:37 -0800

    pressure demo

--- examples/pressure/script.js
+++ examples/pressure/script.js
@@ -1,26 +1,78 @@
 import BS from "../../build/brilliantsole.module.js";
 window.BS = BS;
 console.log({ BS });
+BS.setAllConsoleLevelFlags({ log: false });
 
-const insole = new BS.Device();
-console.log({ insole });
-window.insole = insole;
+BS.Device.InsoleSides.forEach((side) => {
+    const insoleContainer = document.querySelector(`.insole.${side}`);
 
-const sides = ["left", "right"];
-const pressureSensorElements = {
-    left: Array.from(document.querySelectorAll(".insole.left [data-pressure]")),
-    right: Array.from(document.querySelectorAll(".insole.left [data-pressure]")),
-};
+    /** @type {HTMLElement[]} */
+    const pressureSensorElements = Array.from(insoleContainer.querySelectorAll("[data-pressure]"));
+    const insole = new BS.Device();
 
-sides.forEach((side) => {
-    pressureSensorElements[side] = Array.from(document.querySelectorAll(`.insole.${side} [data-pressure]`)).sort(
-        (a, b) => a.dataset.pressure - b.dataset.pressure
-    );
+    /** @type {HTMLButtonElement} */
+    const toggleConnectionButton = insoleContainer.querySelector(".toggleConnection");
+    toggleConnectionButton.addEventListener("click", () => {
+        if (insole.isConnected) {
+            insole.disconnect();
+        } else {
+            insole.connect();
+        }
+    });
+    insole.addEventListener("connected", () => {
+        if (insole.insoleSide != side) {
+            console.error(`wrong insole side - insole must be "${side}", got "${insole.insoleSide}"`);
+            insole.disconnect();
+        }
+    });
+    insole.addEventListener("connectionStatus", () => {
+        switch (insole.connectionStatus) {
+            case "connected":
+                toggleConnectionButton.innerHTML = "disconnect";
+                toggleConnectionButton.disabled = false;
+                break;
+            case "not connected":
+                toggleConnectionButton.innerHTML = "connect";
+                toggleConnectionButton.disabled = false;
+                break;
+            case "connecting":
+            case "disconnecting":
+                toggleConnectionButton.innerHTML = insole.connectionStatus;
+                toggleConnectionButton.disabled = true;
+                break;
+        }
+    });
+
+    /** @type {HTMLButtonElement} */
+    const togglePressureDataButton = insoleContainer.querySelector(".togglePressureData");
+    togglePressureDataButton.addEventListener("click", () => {
+        const isPressureDataEnabled = insole.sensorConfiguration.pressure > 0;
+        if (isPressureDataEnabled) {
+            insole.setSensorConfiguration({ pressure: 0 });
+            togglePressureDataButton.innerText = "disabling pressure data...";
+        } else {
+            insole.setSensorConfiguration({ pressure: 20 });
+            togglePressureDataButton.innerText = "enabling pressure data...";
+        }
+        togglePressureDataButton.disabled = true;
+    });
+    insole.addEventListener("getSensorConfiguration", () => {
+        const isPressureDataEnabled = insole.sensorConfiguration.pressure > 0;
+        if (isPressureDataEnabled) {
+            togglePressureDataButton.innerText = "disable pressure data";
+        } else {
+            togglePressureDataButton.innerText = "enable pressure data";
+        }
+        togglePressureDataButton.disabled = false;
+    });
+
+    insole.addEventListener("pressure", (event) => {
+        /** @type {import("../../build/brilliantsole.module.js").BrilliantSolePressureData} */
+        const pressure = event.message.pressure;
+        pressure.sensors.forEach((sensor, index) => {
+            pressureSensorElements[index].style.opacity = sensor.normalizedValue * window.normalizedValueScalar;
+        });
+    });
 });
-console.log({ pressureSensorElements });
 
-insole.addEventListener("pressure", (event) => {
-    /** @type {import("../../build/brilliantsole.module.js").BrilliantSolePressureData} */
-    const pressure = event.message.pressure;
-    pressure.sensors.forEach((sensor) => {});
-});
+window.normalizedValueScalar = 15;
